d6beea251912ee41247b7e278840ef46679b065a
Devansh Shah <86820517+shahdevansh28@users.noreply.github.com>
2023-03-13 17:32:32 +0530

    login api

---
+++ HR_Management_System/HR_Management_System/Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+﻿using HR_Management_System.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR_Management_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        private readonly HRMContext _context;
+        public AuthController(HRMContext context)
+        {
+            _context = context;
+        }
+        /*public IActionResult Index()
+        {
+            return View();
+        }*/
+        [HttpPost("Login")]
+        public async Task<ActionResult> Login(EmployeeLoginDTO empDTO)
+        {
+            //var res = await _authRepo.Login(userDTO.Username, userDTO.Password);
+            var user = await _context.Employees.FirstOrDefaultAsync(u => u.Username.ToLower() == empDTO.Username.ToLower());
+
+            if (user == null)
+            {
+                return BadRequest($"Incorrect username or password!");
+            }/*
+            else if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+            {
+                return null;
+            }*/
+            /*else
+            {
+                //Console.WriteLine(user);
+                return CreateToken(user);
+            }*/
+            Console.WriteLine(user);
+            /*if (res == null)
+            {
+                return BadRequest($"Incorrect username or password!");
+            }*/
+            return Ok(new { token = user.Id, status = 200 });
+        }
+    }
+}
---
+++ HR_Management_System/HR_Management_System/Migrations/20230310121528_seconds.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HR_Management_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class seconds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Leaves_Employees_EmployeeId",
+                table: "Leaves");
+
+            migrationBuilder.RenameColumn(
+                name: "EmployeeId",
+                table: "Leaves",
+                newName: "EmployeeID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Leaves_EmployeeId",
+                table: "Leaves",
+                newName: "IX_Leaves_EmployeeID");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "EmployeeID",
+                table: "Leaves",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Leaves_Employees_EmployeeID",
+                table: "Leaves",
+                column: "EmployeeID",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Leaves_Employees_EmployeeID",
+                table: "Leaves");
+
+            migrationBuilder.RenameColumn(
+                name: "EmployeeID",
+                table: "Leaves",
+                newName: "EmployeeId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Leaves_EmployeeID",
+                table: "Leaves",
+                newName: "IX_Leaves_EmployeeId");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "EmployeeId",
+                table: "Leaves",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Leaves_Employees_EmployeeId",
+                table: "Leaves",
+                column: "EmployeeId",
+                principalTable: "Employees",
+                principalColumn: "Id");
+        }
+    }
+}
